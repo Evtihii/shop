e868e5d52ed90c7c4e8f356b5f3a8f6df4f58bdf
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2022-07-12 16:51:31 +0300

    task1-comments

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -6,11 +6,18 @@ import { Product } from 'src/app/product/models/product.model';
   providedIn: 'root'
 })
 export class CartService {
+  // тут не понял, почему это свойство является необязательным, если с ним ниже работа идет
+  // в чем цель?
   productsInCart?: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
   constructor() { }
 
-  addToCart(product: Product) {
-    this.productsInCart?.next([...this.productsInCart.value, product])
+  // можно немного усовершенствовать метод
+  // 1. передавать несколько продуктов
+  // 2. использовать метод в цепочке
+  // это в целях демо
+  addToCart(...products: Product[]): this {
+    this.productsInCart?.next([...this.productsInCart.value, ...products]);
+    return this;
   }
 
   totalCart() {
